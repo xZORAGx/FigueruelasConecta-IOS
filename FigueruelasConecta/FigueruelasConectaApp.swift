9e87a34fc0a96970dfffcef4a72a50222a18260b
xZORAGx <drogeralvarez@gmail.com>
2025-06-19 00:52:45 +0200

    Initial Commit

---
+++ FigueruelasConecta/FigueruelasConectaApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct FigueruelasConectaApp: App {
+    // 🔗 Conecta AppDelegate con SwiftUI
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FigueruelasConectaTests/FigueruelasConectaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FigueruelasConectaTests.swift
+//  FigueruelasConectaTests
+//
+//  Created by David Roger Alvarez on 15/6/25.
+//
+
+import Testing
+@testable import FigueruelasConecta
+
+struct FigueruelasConectaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
